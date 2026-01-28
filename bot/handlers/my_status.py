c6cd53a96aa6855905e5c7b0dd66d3b479db2ee1
alikurb12 <gurbansahedovali@gmail.com>
2026-01-28 20:28:16 +0300

    reformat date in status

--- bot/handlers/my_status.py
+++ bot/handlers/my_status.py
@@ -8,18 +8,21 @@ router = Router()
 @router.callback_query(F.data == "my_status")
 async def show_my_status(callback_query: CallbackQuery):
     user_data = await UsersDAO.get_by_id(user_id=callback_query.from_user.id)
+    date_str = user_data.subscription_end.strftime('%d.%m.%Y')
     if user_data.subscription_type == "refferal":
         await callback_query.message.edit_text(
-            text="Ваш тип подписки: Реферальная\n"
-                 f"Дата окончания подписки: {user_data.subscription_end}\n"
+            text="Ваш тип подписки: <b>Реферальная</b>\n"
+                 f"Дата окончания подписки: {date_str}\n"
                  f"Реферальный UUID: {user_data.refferal_uuid}",
-            reply_markup=kb.my_status_kb 
+            reply_markup=kb.my_status_kb,
+            parse_mode="HTML"
         )
     elif user_data.subscription_type == "standard":
         await callback_query.message.edit_text(
-            text="Ваш тип подписки: Платная\n"
-                 f"Дата окончания подписки: {user_data.subscription_end}\n",
-            reply_markup=kb.my_status_kb
+            text="Ваш тип подписки: <h1>Платная</h1>\n"
+                 f"Дата окончания подписки: {date_str}\n",
+            reply_markup=kb.my_status_kb,
+            parse_mode="HTML"
         )
     else:
         await callback_query.message.edit_text(
