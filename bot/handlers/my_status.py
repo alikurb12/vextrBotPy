0d73a47933e807a29af8892d621948d1f97475b1
alikurb12 <gurbansahedovali@gmail.com>
2026-01-28 20:31:06 +0300

    refactor

--- bot/handlers/my_status.py
+++ bot/handlers/my_status.py
@@ -7,8 +7,10 @@ router = Router()
 
 @router.callback_query(F.data == "my_status")
 async def show_my_status(callback_query: CallbackQuery):
+    
     user_data = await UsersDAO.get_by_id(user_id=callback_query.from_user.id)
     date_str = user_data.subscription_end.strftime('%d.%m.%Y')
+    
     if user_data.subscription_type == "refferal":
         await callback_query.message.edit_text(
             text="Ваш тип подписки: <b>Реферальная</b>\n"
@@ -17,6 +19,7 @@ async def show_my_status(callback_query: CallbackQuery):
             reply_markup=kb.my_status_kb,
             parse_mode="HTML"
         )
+    
     elif user_data.subscription_type == "standard":
         await callback_query.message.edit_text(
             text="Ваш тип подписки: <h1>Платная</h1>\n"
@@ -24,6 +27,7 @@ async def show_my_status(callback_query: CallbackQuery):
             reply_markup=kb.my_status_kb,
             parse_mode="HTML"
         )
+    
     else:
         await callback_query.message.edit_text(
             text="У вас нет активной подписки.",
