b59ff7f03d8361f616f8179d2ecf70e0c2bcb5a7
alikurb12 <gurbansahedovali@gmail.com>
2026-03-03 13:39:53 +0300

    add new file setting tp order

---
+++ backend/exchange_apis/okx/services/set_tp_order.py
@@ -0,0 +1,14 @@
+import okx.Trade as Trade
+
+async def set_tp_order(
+        api_key : str,
+        secret_key : str,
+        passphrase : str,
+        symbol : str,
+        position_side : str,
+        tp_price : float,
+        quantity : str,
+):
+    #TODO add function for placing take-profit order
+
+    pass
