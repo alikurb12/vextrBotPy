16368de447edce427571738f123150f4039a0e27
alikurb12 <gurbansahedovali@gmail.com>
2026-03-16 22:13:44 +0300

    change some atributes to be optional in SignalShema

--- backend/utils/signal_shema.py
+++ backend/utils/signal_shema.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class SignalSchema(BaseModel):
     action : str
     symbol : str
     price : float
     stop_loss : float
-    take_profit_1 : float
-    take_profit_2 : float
-    take_profit_3 : float
+    take_profit_1 : Optional[float] = None
+    take_profit_2 : Optional[float] = None
+    take_profit_3 : Optional[float] = None
