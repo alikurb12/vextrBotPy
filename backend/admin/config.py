8fcff089e91b923822f1ba990707a7dec5a712b3
alikurb12 <gurbansahedovali@gmail.com>
2026-03-19 17:49:26 +0300

    add trades model for admin panel

--- backend/admin/config.py
+++ backend/admin/config.py
@@ -1,5 +1,6 @@
 from sqladmin import Admin
 
 def configure_admin_routes(admin : Admin):
-    from backend.admin.models import UserAdmin
+    from backend.admin.models import UserAdmin, TradesAdmin
     admin.add_view(UserAdmin)
+    admin.add_view(TradesAdmin)
--- backend/admin/models.py
+++ backend/admin/models.py
@@ -1,5 +1,6 @@
 from sqladmin import ModelView
 from database.models.users.models import Users
+from database.models.trades.models import Trades
 
 class UserAdmin(ModelView, model=Users):
     name = "Пользователь"
@@ -11,4 +12,21 @@ class UserAdmin(ModelView, model=Users):
         Users.api_key,
         Users.secret_key,
         Users.subscription_type,
+    ]
+
+
+class TradesAdmin(ModelView, model=Trades):
+    name = "Сделка"
+    name_plural = "Сделки"
+    column_list = [
+        Trades.trade_id,
+        Trades.user_id,
+        Trades.symbol,
+        Trades.side,
+        Trades.entry_price,
+        Trades.stop_loss,
+        Trades.take_profit_1,
+        Trades.take_profit_2,
+        Trades.take_profit_3,
+        Trades.status,
     ]
